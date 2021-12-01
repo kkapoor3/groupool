e9d7a6ee4da3bfe9293fbd5a506e6e69e21ded2b
Harsha2871 <harshasudheer9@gmail.com>
2021-11-30 21:32:26 -0600

    changed color of app bar

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -128,8 +128,9 @@ class _SearchState extends State<Search> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Connect"),
+        backgroundColor: Colors.brown[900],
       ),
-      backgroundColor: Colors.brown[900],
+
       body: isLoading
           ? Container(
               child: Center(
